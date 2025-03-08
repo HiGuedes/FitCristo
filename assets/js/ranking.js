7520991e374fdf1d0aa92f6709e7ed9fa0513498
Luiz Carlos <luizcarloswebti@gmail.com>
2025-03-08 17:36:45 -0300

    Criação do ranking.js e modificações em painel em ranking.html

---
+++ assets/js/ranking.js
@@ -0,0 +1,16 @@
+async function carregarMelhorParticipante() {
+    const url = 'https://script.google.com/macros/s/AKfycbwgyiURHioe_dIWihaAGVj-aTV-S0vuF7ytj0QTJDKe3kTVcJa47REPqKkdnyLKpHkw/exec'; // Substitua pela URL da sua API
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Supondo que o nome do melhor participante esteja na primeira célula da planilha
+        const nomeMelhor = data[0][0];
+        document.getElementById('nome-melhor').textContent = nomeMelhor;
+    } catch (error) {
+        console.error('Erro ao carregar o melhor participante:', error);
+    }
+}
+
+carregarMelhorParticipante();
